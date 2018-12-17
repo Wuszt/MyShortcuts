e6fd88475a86688d641bb57b774e01b8ec72e3fa
Wuszt <igoh4ni@gmail.com>
2018-12-17 14:58:14 +0100

    Bat tweaks.

--- MyShortcuts.bat
+++ MyShortcuts.bat
@@ -7,47 +7,12 @@ if exist "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\%~nx0" EX
 @ECHO OFF
 setlocal EnableDelayedExpansion
 
-NET FILE 1>NUL 2>NUL
-if '%errorlevel%' == '0' ( goto START ) else ( goto getPrivileges ) 
-
-:getPrivileges
-if '%1'=='ELEV' ( goto START )
-
-set "linkDestination=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\%~nx0"
-set "linkTargets=%~f0"
-
-::Add quotes to the batch path, if needed
-set "script=%0"
-set script=%script:"=%
-IF '%0'=='!script!' ( GOTO PathQuotesDone )
-    set "linkDestination=""%linkDestination%"""
-:PathQuotesDone
-
-::Add quotes to the arguments, if needed.
-:ArgLoop
-IF '%1'=='' ( GOTO EndArgLoop ) else ( GOTO AddArg )
-    :AddArg
-    set "arg=%1"
-    set arg=%arg:"=%
-    IF '%1'=='!arg!' ( GOTO NoQuotes )
-        set "linkTargets=%linkTargets% "%1""
-        GOTO QuotesDone
-        :NoQuotes
-        set "linkTargets=%linkTargets% %1"
-    :QuotesDone
-    shift
-    GOTO ArgLoop
-:EndArgLoop
+set "linkDestination=""C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\%~nx0"""
+set "linkTarget=""%~f0"""
 
 ::Create and run the vb script to elevate the batch file
 ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs"
-ECHO UAC.ShellExecute "cmd", "/c mklink !linkDestination! !linkTargets!", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
+ECHO UAC.ShellExecute "cmd", "/c mklink !linkDestination! !linkTarget!", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
 "%temp%\OEgetPrivileges.vbs" 
-exit /B
-
-:START
-::Remove the elevation tag and set the correct working directory
-IF '%1'=='ELEV' ( shift /1 )
-cd /d %~dp0
-
+del "%temp%\OEgetPrivileges.vbs"
 EXIT
