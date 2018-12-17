fc9bca85983755754b572fcc7f1960113801da32
Wuszt <igoh4ni@gmail.com>
2018-12-17 17:13:16 +0100

    Bat tweaks.

--- MyShortcuts.bat
+++ MyShortcuts.bat
@@ -9,10 +9,10 @@ setlocal EnableDelayedExpansion
 
 set "linkDestination=""C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\%~nx0"""
 set "linkTarget=""%~f0"""
+set "cd=cd ""%cd%"""
 
 ::Create and run the vb script to elevate the batch file
 ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs"
-ECHO UAC.ShellExecute "cmd", "/c mklink !linkDestination! !linkTarget!", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
+ECHO UAC.ShellExecute "cmd", "/c echo !cd! > !linkDestination! & echo !linkTarget! >> !linkDestination! >> !linkDestination!", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
 "%temp%\OEgetPrivileges.vbs" 
-del "%temp%\OEgetPrivileges.vbs"
 EXIT
